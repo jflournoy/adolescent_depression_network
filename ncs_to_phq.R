111cf38d9d1d414a4cde4f0f7e26d30819899591
Jolanda Kossakowski <mail@jolandakossakowski.eu>
2018-11-07 16:57:09 -0500

    update data
    updated data

--- ncs_to_phq.R
+++ ncs_to_phq.R
@@ -32,16 +32,29 @@ for(i in 1:ncol(ncsdata))
 #ncsdata_num[is.na(ncsdata_num)] = 0
 
 # converting to PHQ
-PHQ1 <- rowSums(cbind(ncsdata_num$D24e, ncsdata_num$D24f), na.rm = TRUE)
-PHQ2 <- rowSums(cbind(ncsdata_num$D24a, ncsdata_num$D24b, ncsdata_num$D24c, ncsdata_num$D24d), na.rm = TRUE)
-PHQ3 <- ifelse((ncsdata_num$D26g == 1 | ncsdata_num$D26h == 1 | ncsdata_num$D26i == 1), 1, 0)
+
+
+PHQ1 <- ifelse(rowSums(is.na(cbind(ncsdata_num$D24e, ncsdata_num$D24f))) == ncol(cbind(ncsdata_num$D24e, ncsdata_num$D24f)), NA,  rowSums(cbind(ncsdata_num$D24e, ncsdata_num$D24f), na.rm = TRUE))
+
+PHQ2 <- ifelse(rowSums(is.na(cbind(ncsdata_num$D24a, ncsdata_num$D24b, ncsdata_num$D24c, ncsdata_num$D24d))) == ncol(cbind(ncsdata_num$D24a, ncsdata_num$D24b, ncsdata_num$D24c, ncsdata_num$D24d)), NA,  rowSums(cbind(ncsdata_num$D24a, ncsdata_num$D24b, ncsdata_num$D24c, ncsdata_num$D24d), na.rm = TRUE))
+
+PHQ3 <- ifelse(rowSums(is.na(cbind(ncsdata_num$D26g, ncsdata_num$D26h, ncsdata_num$D26i))) == ncol(cbind(ncsdata_num$D26g, ncsdata_num$D26h, ncsdata_num$D26i)), NA, ifelse((ncsdata_num$D26g == 1 | ncsdata_num$D26h == 1 | ncsdata_num$D26i == 1), 1, 0))
+
 PHQ4 <- ncsdata_num$D26j
-PHQ5 <-  ifelse((ncsdata_num$D26a == 1 | ncsdata_num$D26b == 1), 1, 0) 
-PHQ6 <- rowSums(cbind(ncsdata_num$D26t, ncsdata_num$D26u, ncsdata_num$D26v, ncsdata_num$D26w), na.rm = TRUE)
-PHQ7 <- rowSums(cbind(ncsdata_num$D26p, ncsdata_num$D26q, ncsdata_num$D26r, ncsdata_num$D26s), na.rm = TRUE)
-PHQ8 <- rowSums(cbind(ncsdata_num$D26l, ncsdata_num$D26m, ncsdata_num$D26n, ncsdata_num$D26o), na.rm = TRUE)
-PHQ9 <- rowSums(cbind(ncsdata_num$D26aa, ncsdata_num$D26bb, ncsdata_num$D26cc, ncsdata_num$D26dd, ncsdata_num$D26ee), na.rm = TRUE)
+
+PHQ5 <-  ifelse(rowSums(is.na(cbind(ncsdata_num$D26a, ncsdata_num$D26b))) == ncol(cbind(ncsdata_num$D26a, ncsdata_num$D26b)), NA, ifelse((ncsdata_num$D26a == 1 | ncsdata_num$D26b == 1), 1, 0))
+
+PHQ6 <- ifelse(rowSums(is.na(cbind(ncsdata_num$D26t, ncsdata_num$D26u, ncsdata_num$D26v, ncsdata_num$D26w))) == ncol(cbind(ncsdata_num$D26t, ncsdata_num$D26u, ncsdata_num$D26v, ncsdata_num$D26w)), NA,  rowSums(cbind(ncsdata_num$D26t, ncsdata_num$D26u, ncsdata_num$D26v, ncsdata_num$D26w), na.rm = TRUE))
+
+PHQ7 <- ifelse(rowSums(is.na(cbind(ncsdata_num$D26p, ncsdata_num$D26q, ncsdata_num$D26r, ncsdata_num$D26s))) == ncol(cbind(ncsdata_num$D26p, ncsdata_num$D26q, ncsdata_num$D26r, ncsdata_num$D26s)), NA,  rowSums(cbind(ncsdata_num$D26p, ncsdata_num$D26q, ncsdata_num$D26r, ncsdata_num$D26s), na.rm = TRUE))
+
+PHQ8 <- ifelse(rowSums(is.na(cbind(ncsdata_num$D26l, ncsdata_num$D26m, ncsdata_num$D26n, ncsdata_num$D26o))) == ncol(cbind(ncsdata_num$D26l, ncsdata_num$D26m, ncsdata_num$D26n, ncsdata_num$D26o)), NA,  rowSums(cbind(ncsdata_num$D26l, ncsdata_num$D26m, ncsdata_num$D26n, ncsdata_num$D26o), na.rm = TRUE))
+
+PHQ9 <- ifelse(rowSums(is.na(cbind(ncsdata_num$D26aa, ncsdata_num$D26bb, ncsdata_num$D26cc, ncsdata_num$D26dd, ncsdata_num$D26ee))) == ncol(cbind(ncsdata_num$D26aa, ncsdata_num$D26bb, ncsdata_num$D26cc, ncsdata_num$D26dd, ncsdata_num$D26ee)), NA,  rowSums(cbind(ncsdata_num$D26aa, ncsdata_num$D26bb, ncsdata_num$D26cc, ncsdata_num$D26dd, ncsdata_num$D26ee), na.rm = TRUE))
+
 
 PHQ_data <- cbind("AGE" = ncs$Age, PHQ1, PHQ2, PHQ3, PHQ4, PHQ5, PHQ6, PHQ7, PHQ8, PHQ9)
 
 write.table(PHQ_data, file = "PHQ_data.txt", row.names = FALSE, col.names = colnames(PHQ_data))
+
+PHQ <- read.table("PHQ_data.txt", header = TRUE)
